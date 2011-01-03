2e12ba78951cbd3da406b01586bcb38da7197d5d
jigsaw@gmail.com <jigsaw@gmail.com@223a9707-4dbd-0305-5f7a-c37a2ca4812f>
2011-01-03 20:37:18 +0000

    bug fix for memcpy rebootex.bin. more comment in code.

--- trunk/6.20/main.c
+++ trunk/6.20/main.c
@@ -41,21 +41,9 @@ static int
 rebootex_callback(unsigned int a1, unsigned int a2, unsigned int a3, 
 		unsigned int a4, unsigned int a5)
 {
-	char *s, *s2;
-
-	s = (char *) 0x88FC0000;
-	s2 = (char *) (rebootex_bin + 0x77040000 + 0x88FC0000);
-	while (s < (char *) 0x88FC71F0) {
-		*s = *s2;
-		s++;
-		s2++;
-	}
-
-	s = (char *) 0x88FB0000;
-	while (s < (char *) 0x88FB0100) {
-		*s = 0;
-		s++;
-	}
+	/* XXX what is 0x77040000 ??? */
+	memcpy((void *) 0x88FC0000, rebootex_bin + 0x77040000 + 0x88FC0000, size_rebootex_bin);
+	memset((void *) 0x88FB0000, 0, 256);
 
 	_sw(size_rebootex_bin, 0x88FB0004);
 	_sw(model, 0x88FB0000);
@@ -87,11 +75,48 @@ power_callback(void)
 	m ^= 4;
 
 	MAKE_CALL(addr + ((m == 0) ? 0x2F28 : 0x2CD8), rebootex_callback);
-	_sw(0x3C0188FC, addr + ((m == 0) ? 0x2F74 : 0x2D24));
-	_sw(0xACC24230, 0x8800CCB0);
-	_sw(0x0A003322, 0x8800CCB4);
-	_sw(0x00001021, 0x8800CCB8);
-	_sw(0x3C058801, 0x8800CCBC);
+
+	/* In PSP 3000, 0x00002D24 of module loadexec is as below:
+	 
+		0x00002D18: 0x02402021 '! @.' - addu       $a0, $s2, $zr
+		0x00002D1C: 0x02A02821 '!(..' - addu       $a1, $s5, $zr
+		0x00002D20: 0x02603021 '!0`.' - addu       $a2, $s3, $zr
+		0x00002D24: 0x3C018860 '`..<' - lui        $at, 0x8860
+		0x00002D28: 0x0020F809 '.. .' - jalr       $at, $ra
+		0x00002D2C: 0x03C03821 '!8..' - addu       $a3, $fp, $zr
+
+	   It is entrance to reboot.bin.
+	   This line replaces entry to reboot.bin with 0x88FC0000, which
+	   rebootex locates.
+	*/
+	_sw(0x3C0188FC, addr + ((m == 0) ? 0x2F74 : 0x2D24)); /* lui $at, 0x88FC */
+
+	/* Somehow prxtool doesn't work with sysmem.prx =(
+	 * But lucky that Davee posted on his blog:
+
+		0x0000CCB0: 0xACC205B0 '....' - sw         $v0, 1456($a2)
+		0x0000CCB4: 0x08003322 '"3..' - j          loc_0000CC88
+		0x0000CCB8: 0x00001021 '!...' - move       $v0, $zr
+ 
+		; ======================================================
+		; Subroutine sceKernelPowerLockForUser - Address 0x0000CCBC - Aliases: sceKernelPowerLock
+		; Exported in sceSuspendForKernel
+		; Exported in sceSuspendForUser
+		sceKernelPowerLockForUser:
+			0x0000CCBC: 0x3C050000 '...<' - lui        $a1, 0x0
+			0x0000CCC0: 0x8CA305B4 '....' - lw         $v1, 1460($a1)
+			0x0000CCC4: 0x27BDFFF0 '...'' - addiu      $sp, $sp, -16
+			0x0000CCC8: 0xAFBF0000 '....' - sw         $ra, 0($sp)
+			0x0000CCCC: 0x14600004 '..`.' - bnez       $v1, loc_0000CCE0
+			0x0000CCD0: 0x00001021 '!...' - move       $v0, $zr
+			0x0000CCD4: 0x8FBF0000 '....' - lw         $ra, 0($sp)
+			 
+	   So the code below is to swap code from CCB0 to CCBC.
+	*/
+	_sw(0xACC24230, 0x8800CCB0); /* sw v0, 0x4230(a2) */
+	_sw(0x0A003322, 0x8800CCB4); /* j 0x02003322 */
+	_sw(0x00001021, 0x8800CCB8); /* addu $v0, $zr, $zr */
+	_sw(0x3C058801, 0x8800CCBC); /* lui $a1, 0x8801 */
 
 	func_rebootex = (void *) addr;
 
@@ -136,6 +161,7 @@ main(void)
 
 	log("start...\n");
 
+	/* search for "sceVshHV" */
 	do {
 		if ((*address_low == 0x56656373) && (*address_high == 0x56486873))
 			break;
@@ -172,10 +198,8 @@ main(void)
 	log("power register done\n");
 	clear_cache();
 
-	p = (unsigned int *) 0x08800010;
-	*p = (unsigned int) power_callback;
-	p = (unsigned int *) 0x08804234;
-	*p = 0x08800000;
+	_sw((unsigned int) power_callback, 0x08800010);
+	_sw(0x08800000, 0x08804234);
 	clear_cache();
 
 	log("suspend intr\n");
